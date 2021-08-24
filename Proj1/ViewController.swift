cea24852ec1cfd01e226a3f37edef9e08e781cee
rochagi <grocha0507@gmail.com>
2021-08-24 20:46:33 -0300

    iniciando projeto de aplicativo de imagens - Reconhecimento familiar infantil

---
+++ Proj1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Proj1
+//
+//  Created by Giovanna Rocha on 24/08/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("IMG"){
+                //load pics
+                pictures.append(item)
+             }
+        }
+        
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+}
+
